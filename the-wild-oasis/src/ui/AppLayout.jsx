4518a20f479dd430081fbd0b63a3b416295615ff
talal4a <talalxus000@gmail.com>
2025-06-04 20:19:50 +0500

    update

--- the-wild-oasis/src/ui/AppLayout.jsx
+++ the-wild-oasis/src/ui/AppLayout.jsx
@@ -2,18 +2,24 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import styled from "styled-components";
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
 const Main = styled.main`
   background-color: green;
   padding: 4rem 4.8rem 6.4rem;
 `;
 export default function AppLayout() {
   return (
-    <div>
+    <StyledAppLayout>
       <Header />
       <Sidebar />
       <Main>
         <Outlet />
       </Main>
-    </div>
+    </StyledAppLayout>
   );
 }
--- the-wild-oasis/src/ui/Sidebar.jsx
+++ the-wild-oasis/src/ui/Sidebar.jsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
-const SideBar=styled.aside`
-    background-color: blue;
-padding: ;
-`
+const SideBar = styled.aside`
+  background-color: blue;
+  padding: 3, 2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+`;
 export default function Sidebar() {
-  return <aside>Side Bar</aside>;
+  return <SideBar>Side Bar</SideBar>;
 }
