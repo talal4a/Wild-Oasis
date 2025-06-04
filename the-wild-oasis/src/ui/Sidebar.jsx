7372bafcb6cabe82303c59076f4ec477f1242212
talal4a <talalxus000@gmail.com>
2025-06-04 19:49:36 +0500

    update the Sidebar

--- the-wild-oasis/src/ui/Sidebar.jsx
+++ the-wild-oasis/src/ui/Sidebar.jsx
@@ -1,8 +1,7 @@
 import styled from "styled-components";
-
 const SideBar=styled.aside`
     background-color: blue;
-
+padding: ;
 `
 export default function Sidebar() {
   return <aside>Side Bar</aside>;
