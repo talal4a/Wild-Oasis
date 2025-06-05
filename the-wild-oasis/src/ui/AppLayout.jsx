a6827c7a9389d873d9d75618b2f17260f1b8cae9
talal4a <talalxus000@gmail.com>
2025-06-05 19:36:23 +0500

    update

--- the-wild-oasis/src/ui/AppLayout.jsx
+++ the-wild-oasis/src/ui/AppLayout.jsx
@@ -16,6 +16,9 @@ const Main = styled.main`
 const Container = styled.div`
   max-width: 120rem;
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 export default function AppLayout() {
   return (
