1ea0a4f0a3883f35b16448536c2dce11a0cbd239
talal4a <talalxus000@gmail.com>
2025-06-15 19:55:15 +0500

    update

--- the-wild-oasis/src/context/DarkModeContext.jsx
+++ the-wild-oasis/src/context/DarkModeContext.jsx
@@ -1,13 +1,25 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 import { useLocalStorageState } from "./../hooks/useLocalStorageState";
 const DarkModeContext = createContext();
 function DarkModeProvider({ children }) {
   const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+  useEffect(
+    function () {
+      if (isDarkMode) {
+        document.documentElement.classList.add("dark-mode");
+        document.documentElement.classList.remove("light-mode");
+      } else {
+        document.documentElement.classList.add("light-mode");
+        document.documentElement.classList.remove("dark-mode");
+      }
+    },
+    [isDarkMode]
+  );
   function toggleDarkMode() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
   return (
-    <DarkModeContext.Provider value={toggleDarkMode}>
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
       {children}
     </DarkModeContext.Provider>
   );
